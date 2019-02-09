9e8d413998a201019f892122cc2e86febaf61af7
Timur <Mokoto1991@mail.ru>
2019-02-09 22:05:46 +0300

    work 1

--- js/game.js
+++ js/game.js
@@ -1,2 +1,13 @@
-
+(function(){
+    class Game {
+        constructor (){
+            this.lifes = 0;
+            this.points = 0;
+            this.emojes = ['🐭', '🐼', '🐻', '🦊', '🐱', '🐮', '🦁', '🐽', '🐨', '🐰', '🐯'];
+            this.lastHole;
+            this.speed = 2100;
+            this.speedLevel = 1;
+        }
+    }
+});
 
